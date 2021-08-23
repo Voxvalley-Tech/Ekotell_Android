7813edeaa069c0e1652a567286810847a95cd2a7
srilakshmi <srilakshmi.t@voxvalley.com>
2021-08-23 19:35:54 +0530

    fixed(SI-2742)

--- app/src/main/java/com/app/ekottel/Retrofit/ApiClient.java
+++ app/src/main/java/com/app/ekottel/Retrofit/ApiClient.java
@@ -7,13 +7,7 @@ import java.io.IOException;
 import java.security.cert.CertificateException;
 import java.util.concurrent.TimeUnit;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+
 
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
@@ -77,44 +71,8 @@ public class ApiClient {
 
     private static OkHttpClient getUnsafeOkHttpClient() {
         try {
-            // Create a trust manager that does not validate certificate chains
-            final TrustManager[] trustAllCerts = new TrustManager[]{
-                    new X509TrustManager() {
-                        @Override
-                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                        }
-
-                        @Override
-                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                        }
-
-                        @Override
-                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                            return new java.security.cert.X509Certificate[]{};
-                        }
-                    }
-            };
-
-            // Install the all-trusting trust manager
-            final SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-
-            // Create an ssl socket factory with our all-trusting manager
-            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
 
             OkHttpClient.Builder builder = new OkHttpClient.Builder();
-
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
-            builder.hostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    //return  true;
-                    HostnameVerifier hv= HttpsURLConnection.getDefaultHostnameVerifier();
-
-                    return hv.verify(hostname, session);
-                }
-            });
             builder.connectTimeout(100, TimeUnit.SECONDS);
             builder.readTimeout(100, TimeUnit.SECONDS);
             HttpLoggingInterceptor.Level logLevel;
